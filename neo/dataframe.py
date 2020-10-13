6f5af73ce9309f559dcf66d224e7168f67c2d64f
ved93 <vpved93@gmail.com>
2020-10-13 21:28:43 +0530

    added joins utility

---
+++ neo/dataframe.py
@@ -0,0 +1,20 @@
+
+##joins
+
+def check_dim(df1,  df2):
+    print(df1.shape,'   ', df2.shape)
+
+    return True
+
+
+def merge(df1,df2,how='left', on = cols):
+    
+    print(df1.shape,'   ', df2.shape)
+
+    df = df1.merge(df2, how= how, on = cols)
+
+    print(df.shape )
+
+    return df
+
+
