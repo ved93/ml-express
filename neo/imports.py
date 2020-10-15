b98d0d1a34201fcaaee117356d8ab6ac31b5efea
ved93 <vpved93@gmail.com>
2020-10-15 21:15:03 +0530

    added decorators

--- neo/imports.py
+++ neo/imports.py
@@ -7,11 +7,11 @@ import numpy as np
 from datetime import datetime, date
 
 
-# def measure_time(func):
-#     def _measure_time(*args, **kwargs):
-#         t0 = datetime.now()
-#         # print(args,kwarg)
-#         ret = func(*args, **kwargs)
-#         print(str(datetime.now() - t0))
-#         return ret
-#     return _measure_time
+def measure_time(func):
+    def _measure_time(*args, **kwargs):
+        t0 = datetime.now()
+        # print(args,kwarg)
+        ret = func(*args, **kwargs)
+        print(str(datetime.now() - t0))
+        return ret
+    return _measure_time
