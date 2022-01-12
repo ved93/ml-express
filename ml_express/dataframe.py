523b44130c46f6f6cfb6f9bf30787398e272dcf8
ved93 <vpved93@gmail.com>
2022-01-12 16:27:05 +0530

    join func modified

--- ml_express/dataframe.py
+++ ml_express/dataframe.py
@@ -7,11 +7,11 @@ def check_dim(df1,  df2):
     return True
 
 
-def merge(df1,df2,how='left', on = cols):
+def merge(df1,df2,how='left',on='cols'):
     
     print(df1.shape,'   ', df2.shape)
 
-    df = df1.merge(df2, how= how, on = cols)
+    df = df1.merge(df2, how= how, on = on)
 
     print(df.shape )
 
